c340c206ca160630f04fe93df2b2d26a370b17c8
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-30 04:37:24 +0000

    changed version in script
    git-svn-id: https://plugins.svn.wordpress.org/nextgen-facebook/tags/2.1.2@645984 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- nextgen-facebook.php
+++ nextgen-facebook.php
@@ -3,7 +3,7 @@
 Plugin Name: NextGEN Facebook OG
 Plugin URI: http://wordpress.org/extend/plugins/nextgen-facebook/
 Description: Adds Open Graph meta tags for Facebook, G+, LinkedIn, etc., plus sharing buttons for FB, G+, Twitter, LinkedIn, Pinterest, tumblr.
-Version: 2.1.1
+Version: 2.1.2
 Author: Jean-Sebastien Morisset
 Author URI: http://surniaulula.com/
 
