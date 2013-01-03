046c37a2dd04137c6b4acf96fd9f29f4f2ad9a70
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-03 00:59:04 +0000

    added copyright to admin.php and widgets.php
    git-svn-id: https://plugins.svn.wordpress.org/nextgen-facebook/trunk@647274 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/admin.php
+++ lib/admin.php
@@ -1,4 +1,17 @@
 <?php
+/*
+Copyright 2012 - Jean-Sebastien Morisset - http://surniaulula.com/
+
+This script is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 3 of the License, or (at your option) any later
+version.
+
+This script is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details at
+http://www.gnu.org/licenses/.
+*/
 
 class ngfbAdminPanel {
 
