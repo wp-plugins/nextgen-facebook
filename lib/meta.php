7a831fe35f0856b48ed557b2147591ace4bd9165
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-25 01:44:19 +0000

    tag for 5.0rc4
    git-svn-id: https://plugins.svn.wordpress.org/nextgen-facebook/tags/5.0rc4@718044 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/meta.php
@@ -0,0 +1,93 @@
+<?php
+/*
+Copyright 2012-2013 - Jean-Sebastien Morisset - http://surniaulula.com/
+
+This script is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 3 of the License, or (at your option) any later
+version.
+
+This script is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details at
+http://www.gnu.org/licenses/.
+*/
+
+if ( ! defined( 'ABSPATH' ) ) 
+	die( 'Sorry, you cannot call this webpage directly.' );
+
+if ( ! class_exists( 'ngfbMeta' ) ) {
+
+	class ngfbMeta {
+
+		protected $ngfb;		// ngfbPlugin
+		protected $form;	// ngfbForm
+
+		public function __construct( &$ngfb_plugin ) {
+			$this->ngfb =& $ngfb_plugin;
+			$this->ngfb->debug->lognew();
+			$this->enable();
+		}
+
+		public function enable() {
+			if ( is_admin() ) {
+				add_action( 'add_meta_boxes', array( &$this, 'add_metabox' ) );
+				add_action( 'save_post', array( &$this, 'save_options' ) );
+			}
+		}
+
+		public function add_metabox() {
+			foreach ( array( 'post' => 'Post', 'page' => 'Page' ) as $id => $name ) 
+				add_meta_box( NGFB_SHORTNAME . '_meta', 
+					NGFB_FULLNAME . ' - Custom ' . $name . ' Settings', 
+					array( &$this, 'show_metabox' ), $id, 'advanced', 'low' );
+		}
+
+		public function show_metabox( $post ) {
+			$this->ngfb->admin->admin_style();
+			echo '<table class="ngfb-settings">';
+			foreach ( $this->get_rows() as $row )
+				echo '<tr>' . $row . '</tr>';
+			echo '</table>';
+		}
+
+		protected function get_rows() {
+			return array(
+				'<th>Topic</th><td>' . $this->ngfb->pro_msg . '</td>',
+				'<th>Title</th><td>' . $this->ngfb->pro_msg . '</td>',
+				'<th>Description</th><td>' . $this->ngfb->pro_msg . '</td>',
+				'<th>Image ID</th><td>' . $this->ngfb->pro_msg . '</td>',
+				'<th>Image URL</th><td>' . $this->ngfb->pro_msg . '</td>',
+				'<th>Maximum Images</th><td>' . $this->ngfb->pro_msg . '</td>',
+				'<th>Maximum Videos</th><td>' . $this->ngfb->pro_msg . '</td>',
+				'<th>Disable Social Buttons</th><td>' . $this->ngfb->pro_msg . '</td>',
+			);
+		}
+
+		public function get_options( $post_id, $idx = '' ) {
+			if ( ! empty( $idx ) ) return false;
+			else return array();
+		}
+
+		public function get_defaults( $idx = '' ) {
+			$defs = array(
+				'og_art_section' => -1,
+				'og_title' => '',
+				'og_desc' => '',
+				'og_img_id' => '',
+				'og_img_id_pre' => ( empty( $this->ngfb->options['og_def_img_id_pre'] ) ? '' : $this->ngfb->options['og_def_img_id_pre'] ),
+				'og_img_url' => '',
+				'og_img_max' => -1,
+				'og_vid_max' => -1,
+			);
+			if ( ! empty( $idx ) ) return $defs[$idx];
+			else return $defs;
+		}
+
+		public function save_options( $post_id ) {
+			return;
+		}
+	}
+}
+
+?>
---
+++ lib/websites/linkedin.php
@@ -0,0 +1,98 @@
+<?php
+/*
+Copyright 2012-2013 - Jean-Sebastien Morisset - http://surniaulula.com/
+
+This script is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 3 of the License, or (at your option) any later
+version.
+
+This script is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details at
+http://www.gnu.org/licenses/.
+*/
+
+if ( ! defined( 'ABSPATH' ) ) 
+	die( 'Sorry, you cannot call this webpage directly.' );
+
+if ( ! class_exists( 'ngfbAdminLinkedIn' ) && class_exists( 'ngfbAdmin' ) ) {
+
+	class ngfbAdminLinkedIn extends ngfbAdmin {
+
+		protected $ngfb;
+
+		public function __construct( &$ngfb_plugin ) {
+			$this->ngfb =& $ngfb_plugin;
+			$this->ngfb->debug->lognew();
+		}
+
+		public function get_rows() {
+			return array(
+				'<th colspan="2" class="social">LinkedIn</th>',
+				'<td colspan="2" style="height:5px;"></td>',
+				'<th>Add to Excerpt Text</th><td>' . $this->ngfb->admin->form->get_checkbox( 'linkedin_on_the_excerpt' ) . '</td>',
+				'<th>Add to Content Text</th><td>' . $this->ngfb->admin->form->get_checkbox( 'linkedin_on_the_content' ) . '</td>',
+				'<th>Preferred Order</th><td>' . $this->ngfb->admin->form->get_select( 'linkedin_order', range( 1, count( $this->ngfb->social_prefix ) ), 'short' ) . '</td>',
+				'<th>JavaScript in</th><td>' . $this->ngfb->admin->form->get_select( 'linkedin_js_loc', $this->js_locations ) . '</td>',
+				'<th>Counter Mode</th><td>' . $this->ngfb->admin->form->get_select( 'linkedin_counter', 
+					array( 
+						'none' => '',
+						'right' => 'Horizontal',
+						'top' => 'Vertical',
+					)
+				) . '</td>',
+				'<th>Show Zero in Counter</th><td>' . $this->ngfb->admin->form->get_checkbox( 'linkedin_showzero' ) . '</td>',
+				'<td colspan="2"></td>',
+				'<td colspan="2"></td>',
+				'<td colspan="2"></td>',
+				'<td colspan="2"></td>',
+				'<td colspan="2"></td>',
+			);
+		}
+
+	}
+}
+
+if ( ! class_exists( 'ngfbSocialLinkedIn' ) && class_exists( 'ngfbSocial' ) ) {
+
+	class ngfbSocialLinkedIn extends ngfbSocial {
+
+		protected $ngfb;
+
+		public function __construct( &$ngfb_plugin ) {
+			$this->ngfb =& $ngfb_plugin;
+			$this->ngfb->debug->lognew();
+		}
+
+		public function get_html( $atts = array() ) {
+			global $post; 
+			$html = '';
+			$use_post = empty( $atts['is_widget'] ) || is_singular() ? true : false;
+			if ( empty( $atts['url'] ) ) $atts['url'] = $this->ngfb->util->get_sharing_url( 'notrack', null, $use_post );
+			$html = '
+				<!-- LinkedIn Button -->
+				<div ' . $this->get_css( 'linkedin', $atts ) . '>
+				<script type="IN/Share" data-url="' . $atts['url'] . '"';
+
+			if ( ! empty( $this->ngfb->options['linkedin_counter'] ) ) 
+				$html .= ' data-counter="' . $this->ngfb->options['linkedin_counter'] . '"';
+
+			if ( ! empty( $this->ngfb->options['linkedin_showzero'] ) ) 
+				$html .= ' data-showzero="true"';
+
+			$html .= '></script></div>'."\n";
+			$this->ngfb->debug->log( 'returning html (' . strlen( $html ) . ' chars)' );
+			return $html;
+		}
+		
+		public function get_js( $pos = 'id' ) {
+			return  '<script type="text/javascript" id="linkedin-script-' . $pos . '">
+				ngfb_header_js( "linkedin-script-' . $pos . '", "' . $this->ngfb->util->get_cache_url( 'https://platform.linkedin.com/in.js' ) . '" );
+			</script>' . "\n";
+		}
+
+	}
+
+}
+?>
