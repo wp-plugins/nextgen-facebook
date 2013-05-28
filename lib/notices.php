15956df90e5320b8e0e2dd9e0d5ff3e465a2bd05
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-28 11:30:28 +0000

    tag 5.0.1
    git-svn-id: https://plugins.svn.wordpress.org/nextgen-facebook/tags/5.0.1@719330 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/notices.php
@@ -0,0 +1,65 @@
+<?php
+/*
+Copyright 2012-2013 - Jean-Sebastien Morisset - http://surniaulula.com/
+
+This script is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 3 of the License, or (at your option) any later
+version.
+
+This script is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details at
+http://www.gnu.org/licenses/.
+*/
+
+if ( ! defined( 'ABSPATH' ) ) 
+	die( 'Sorry, you cannot call this webpage directly.' );
+
+if ( ! class_exists( 'ngfbNotices' ) ) {
+
+	class ngfbNotices {
+
+		private $ngfb;		// ngfbPlugin
+		private $msgs = array();
+
+		public function __construct( &$ngfb_plugin ) {
+			$this->ngfb =& $ngfb_plugin;
+			$this->ngfb->debug->lognew();
+
+			add_action( 'admin_notices', array( &$this, 'admin_notices' ) );
+		}
+
+		public function err( $msg = '' ) {
+			$this->log( 'err', $msg );
+		}
+
+		public function inf( $msg = '' ) {
+			$this->log( 'inf', $msg );
+		}
+
+		public function log( $type, $msg = '' ) {
+			if ( ! empty( $msg ) ) $this->msgs[$type][] = $msg;
+		}
+
+		public function admin_notices() {
+			$p_start = '<p style="padding:0;margin:5px;"><a href="' . $this->ngfb->util->get_options_url() . '">' . $this->ngfb->menuname . '</a>';
+			$p_end = '</p>';
+
+			if ( ! empty( $this->msgs['err'] ) ) {
+				echo '<div id="message" class="error">';
+				foreach ( $this->msgs['err'] as $msg ) echo $p_start, ' Warning : ', $msg, $p_end;
+				echo '</div>', "\n";
+			}
+
+			if ( ! empty( $this->msgs['inf'] ) ) {
+				echo '<div id="message" class="updated fade">';
+				foreach ( $this->msgs['inf'] as $msg ) echo $p_start, ' Notice : ', $msg, $p_end;
+				echo '</div>', "\n";
+			}
+		}
+
+	}
+
+}
+?>
---
+++ lib/postmeta.php
@@ -0,0 +1,92 @@
+<?php
+/*
+Copyright 2012-2013 - Jean-Sebastien Morisset - http://surniaulula.com/
+
+This script is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 3 of the License, or (at your option) any later
+version.
+
+This script is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details at
+http://www.gnu.org/licenses/.
+*/
+
+if ( ! defined( 'ABSPATH' ) ) 
+	die( 'Sorry, you cannot call this webpage directly.' );
+
+if ( ! class_exists( 'ngfbPostMeta' ) ) {
+
+	class ngfbPostMeta {
+
+		protected $ngfb;	// ngfbPlugin
+
+		// executed by ngfbPostMetaPro() as well
+		// children executing this __construct() should have an empty add_actions() method
+		public function __construct( &$ngfb_plugin ) {
+			$this->ngfb =& $ngfb_plugin;
+			$this->ngfb->debug->lognew();
+			$this->add_actions();
+		}
+
+		private function add_actions() {
+			if ( is_admin() ) {
+				add_action( 'add_meta_boxes', array( &$this, 'add_metaboxes' ) );
+				add_action( 'save_post', array( &$this, 'save_options' ) );
+			}
+		}
+
+		public function add_metaboxes() {
+			foreach ( array( 'post' => 'Post', 'page' => 'Page' ) as $id => $name ) 
+				add_meta_box( NGFB_META_NAME, $this->ngfb->fullname . ' - Custom ' . $name . ' Settings', 
+					array( &$this->ngfb->meta, 'show_metabox' ), $id, 'advanced', 'high' );
+		}
+
+		public function show_metabox( $post ) {
+			$this->ngfb->admin->settings_style();
+			echo '<table class="ngfb-settings">';
+			foreach ( $this->get_rows() as $row )
+				echo '<tr>' . $row . '</tr>';
+			echo '</table>';
+		}
+
+		protected function get_rows() {
+			return array(
+				'<th>Topic</th><td>' . $this->ngfb->msgs['pro_feature'] . '</td>',
+				'<th>Title</th><td>' . $this->ngfb->msgs['pro_feature'] . '</td>',
+				'<th>Description</th><td>' . $this->ngfb->msgs['pro_feature'] . '</td>',
+				'<th>Image ID</th><td>' . $this->ngfb->msgs['pro_feature'] . '</td>',
+				'<th>Image URL</th><td>' . $this->ngfb->msgs['pro_feature'] . '</td>',
+				'<th>Maximum Images</th><td>' . $this->ngfb->msgs['pro_feature'] . '</td>',
+				'<th>Maximum Videos</th><td>' . $this->ngfb->msgs['pro_feature'] . '</td>',
+				'<th>Disable Social Buttons</th><td>' . $this->ngfb->msgs['pro_feature'] . '</td>',
+			);
+		}
+
+		public function get_options( $post_id, $idx = '' ) {
+			if ( ! empty( $idx ) ) return false;
+			else return array();
+		}
+
+		public function get_defaults( $idx = '' ) {
+			$defs = array(
+				'og_art_section' => -1,
+				'og_title' => '',
+				'og_desc' => '',
+				'og_img_id' => '',
+				'og_img_id_pre' => ( empty( $this->ngfb->options['og_def_img_id_pre'] ) ? '' : $this->ngfb->options['og_def_img_id_pre'] ),
+				'og_img_url' => '',
+				'og_img_max' => -1,
+				'og_vid_max' => -1,
+			);
+			if ( ! empty( $idx ) ) return $defs[$idx];
+			else return $defs;
+		}
+
+		public function save_options( $post_id ) {
+		}
+	}
+}
+
+?>
