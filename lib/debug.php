0e8f0a35db2898cf7a477d3b7456f1b9ccf5f621
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-04 18:32:04 +0000

    tag 5.1.1
    git-svn-id: https://plugins.svn.wordpress.org/nextgen-facebook/tags/5.1.1@722708 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/debug.php
@@ -0,0 +1,129 @@
+<?php
+/*
+Copyright 2013 - Jean-Sebastien Morisset - http://surniaulula.com/
+
+This script is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 3 of the License, or (at your option) any later
+version.
+
+This script is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details at
+http://www.gnu.org/licenses/.
+*/
+
+if ( ! defined( 'ABSPATH' ) ) 
+	die( 'Sorry, you cannot call this webpage directly.' );
+
+if ( ! class_exists( 'ngfbDebug' ) ) {
+
+	class ngfbDebug {
+
+		public $on = false;
+		public $logs = array(
+			'html' => false,
+			'wp' => false,
+		);
+		private $ngfb;		// ngfbPlugin
+		private $msgs = array();
+
+		public function __construct( &$ngfb_plugin ) {
+			$this->ngfb =& $ngfb_plugin;
+
+			if ( ! empty( $this->ngfb->options['ngfb_debug'] ) || ( defined( 'NGFB_DEBUG' ) && NGFB_DEBUG ) ) {
+				$this->on = true;
+				$this->logs['html'] = true;
+			}
+
+			// to enable logging in WP's debug log, WP_DEBUG must be true, WP_DEBUG_LOG true, and WP_DEBUG_DISPLAY false.
+			if ( defined( 'WP_DEBUG' ) && WP_DEBUG && defined( 'WP_DEBUG_LOG' ) && WP_DEBUG_LOG && defined( 'WP_DEBUG_DISPLAY' ) && ! WP_DEBUG_DISPLAY )
+				$this->logs['wp'] = true;
+
+			$this->mark();
+		}
+
+		public function mark() {
+			$this->log( 'MARK', 2 );
+		}
+
+		public function log( $var = '', $back = 1 ) {
+			if ( $this->on == true ) {
+				$log_msg = '';
+				$stack = debug_backtrace();
+				if ( ! empty( $stack[$back]['class'] ) ) 
+					$log_msg .= sprintf( '%-25s:: ', $stack[$back]['class'] );
+
+				if ( ! empty( $stack[$back]['function'] ) ) 
+					$log_msg .= sprintf( '%-24s : ', $stack[$back]['function'] );
+
+				if ( is_array( $var ) || is_object( $var ) )
+					$log_msg .= print_r( $var, true );
+				else $log_msg .= $var;
+
+				if ( $this->logs['html'] == true )
+					$this->msgs[] = $log_msg;
+
+				if ( $this->logs['wp'] == true )
+					error_log( 'NGFB ' . $log_msg );
+			}
+		}
+
+		public function show( $data = null, $title = null, $from = null ) {
+			if ( empty( $from ) ) {
+				$stack = debug_backtrace();
+				if ( ! empty( $stack[1]['class'] ) ) 
+					$from .= $stack[1]['class'] . '::';
+				if ( ! empty( $stack[1]['function'] ) )
+					$from .= $stack[1]['function'];
+			}
+			echo $this->get( $data, $title, $from );
+		}
+
+		public function get( $data = null, $title = null, $from = null ) {
+			$html = null;
+			if ( $this->on || ( defined( 'NGFB_DEBUG' ) && NGFB_DEBUG ) ) {
+				$html .= "<!-- " . $this->ngfb->fullname . " debug";
+				if ( empty( $from ) ) {
+					$stack = debug_backtrace();
+					if ( ! empty( $stack[1]['class'] ) ) 
+						$from .= $stack[1]['class'] . '::';
+					if ( ! empty( $stack[1]['function'] ) )
+						$from .= $stack[1]['function'];
+				}
+				if ( empty( $data ) ) {
+					$this->log( 'truncating debug log' );
+					$data = $this->msgs;
+					$this->msgs = array();
+				}
+				if ( ! empty( $from ) ) $html .= ' from ' . $from . '()';
+				if ( ! empty( $title ) ) $html .= ' ' . $title;
+				if ( ! empty( $data ) ) {
+					$html .= ' : ';
+					if ( is_array( $data ) ) {
+						$html .= "\n";
+						$is_assoc = $this->is_assoc( $data );
+						if ( $is_assoc ) ksort( $data );
+						foreach ( $data as $key => $val ) 
+							$html .= $is_assoc ? "\t$key = $val\n" : "\t$val\n";
+						unset ( $key, $val );
+					} else {
+						if ( preg_match( '/^Array/', $data ) ) $html .= "\n";	// check for print_r() output
+						$html .= $data;
+					}
+				}
+				$html .= ' -->' . "\n";
+			}
+			return $html;
+		}
+
+		private function is_assoc( $arr ) {
+			if ( ! is_array( $arr ) ) return false;
+			return is_numeric( implode( array_keys( $arr ) ) ) ? false : true;
+		}
+
+
+	}
+}
+
+?>
---
+++ lib/user.php
@@ -0,0 +1,78 @@
+<?php
+/*
+Copyright 2012-2013 - Jean-Sebastien Morisset - http://surniaulula.com/
+
+This script is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 3 of the License, or (at your option) any later
+version.
+
+This script is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details at
+http://www.gnu.org/licenses/.
+*/
+
+if ( ! defined( 'ABSPATH' ) ) 
+	die( 'Sorry, you cannot call this webpage directly.' );
+
+if ( ! class_exists( 'ngfbUser' ) ) {
+
+	class ngfbUser {
+
+		private $ngfb;		// ngfbPlugin
+
+		public function __construct( &$ngfb_plugin ) {
+			$this->ngfb =& $ngfb_plugin;
+			$this->ngfb->debug->mark();
+
+			add_filter( 'user_contactmethods', array( &$this, 'contactmethods' ), 20, 1 );
+		}
+
+		public function contactmethods( $fields = array() ) { 
+			foreach ( preg_split( '/ *, */', NGFB_CONTACT_FIELDS ) as $field_list ) {
+				$field_name = preg_split( '/ *: */', $field_list );
+				$fields[$field_name[0]] = $field_name[1];
+			}
+			ksort( $fields, SORT_STRING );
+			return $fields;
+		}
+
+		// called from head and opengraph classes
+		public function get_author_url( $author_id, $field_name = 'url' ) {
+			switch ( $field_name ) {
+				case 'none' :
+					break;
+				case 'index' :
+					$url = get_author_posts_url( $author_id );
+					break;
+				default :
+					$url = get_the_author_meta( $field_name, $author_id );
+
+					// if empty or not a URL, then fallback to the author index page
+					if ( $this->ngfb->options['og_author_fallback'] && ( empty( $url ) || ! preg_match( '/:\/\//', $url ) ) )
+						$url = get_author_posts_url( $author_id );
+
+					break;
+			}
+			return $url;
+		}
+
+		public function collapse_metaboxes( $page, $ids = array(), $force = false ) {
+			$user_id = get_current_user_id();
+			$option_name = 'closedpostboxes_' . $page;
+			$option_arr = get_user_option( $option_name, $user_id );
+
+			if ( ! is_array( $option_arr ) )
+				$option_arr = array();
+
+			if ( empty( $option_arr ) || $force == true )
+				foreach ( $ids as $id ) $option_arr[] = $page . '_' . $id;
+
+			update_user_option( $user_id, $option_name, array_unique( $option_arr ), true );
+		}
+
+	}
+
+}
+?>
