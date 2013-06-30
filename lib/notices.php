606ef65a063609c96612a8c8f69d41a3fc7ff885
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-30 23:17:23 +0000

    small tweak to update message
    git-svn-id: https://plugins.svn.wordpress.org/nextgen-facebook/tags/6.1@734421 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/notices.php
+++ lib/notices.php
@@ -52,6 +52,7 @@ if ( ! class_exists( 'ngfbNotices' ) ) {
 						border:1px dashed #ccc;
 						padding:10px 40px 10px 40px;
 						overflow:hidden;
+						line-height:1.2em;
 					}
 				</style>';
 				foreach ( $this->log['nag'] as $msg )
