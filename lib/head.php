bdad76dbd2cfb658f01f1f83eb3b70f72339b0ce
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-03 17:29:20 +0000

    added ob_end_flush to head.php
    git-svn-id: https://plugins.svn.wordpress.org/nextgen-facebook/trunk@735554 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/head.php
+++ lib/head.php
@@ -55,6 +55,7 @@ if ( ! class_exists( 'ngfbHead' ) ) {
 		public function html( $html_tags = array() ) {
 			global $post;
 			$author_url = '';
+			@ob_end_flush();	// just in case another plugin left the output buffer open
 		
 			echo "\n<!-- ", $this->ngfb->fullname, " meta tags BEGIN -->\n";
 
