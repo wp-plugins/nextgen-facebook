b516dd9709f3e82a78f7b55a8308b5b9508f87b7
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-06 21:35:33 +0000

    move style/script reg in enqueue in resp to #17916
    git-svn-id: https://plugins.svn.wordpress.org/nextgen-facebook/tags/6.2@737188 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/script.php
+++ lib/script.php
@@ -17,20 +17,15 @@ if ( ! class_exists( 'ngfbScript' ) ) {
 		public function __construct( &$ngfb_plugin ) {
 			$this->ngfb =& $ngfb_plugin;
 			$this->ngfb->debug->mark();
-			$this->register_scripts();
 
 			add_action( 'admin_enqueue_scripts', array( &$this, 'admin_enqueue_scripts' ) );
 		}
 
-		public function register_scripts() {
-			if ( is_admin() ) {
-				wp_register_script( 'jquery-qtip', NGFB_URLPATH . 'js/jquery-qtip.min.js', array( 'jquery' ), '1.0.0-RC3', true );
-				wp_register_script( $this->ngfb->acronym . '_tooltips', NGFB_URLPATH . 'js/jquery-tooltips.min.js', false, $this->ngfb->version, true );
-				wp_register_script( $this->ngfb->acronym . '_postmeta', NGFB_URLPATH . 'js/jquery-postmeta.min.js', false, $this->ngfb->version, true );
-			}
-		}
-
 		public function admin_enqueue_scripts( $hook ) {
+			wp_register_script( 'jquery-qtip', NGFB_URLPATH . 'js/jquery-qtip.min.js', array( 'jquery' ), '1.0.0-RC3', true );
+			wp_register_script( $this->ngfb->acronym . '_tooltips', NGFB_URLPATH . 'js/jquery-tooltips.min.js', false, $this->ngfb->version, true );
+			wp_register_script( $this->ngfb->acronym . '_postmeta', NGFB_URLPATH . 'js/jquery-postmeta.min.js', false, $this->ngfb->version, true );
+
 			// don't load our javascript where we don't need it
 			switch ( $hook ) {
 				case 'post.php' :
