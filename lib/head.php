305066001673d940569372f26e4441e3c1a6a6d4
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-11 14:50:31 +0000

    adding tag 6.2.2
    git-svn-id: https://plugins.svn.wordpress.org/nextgen-facebook/tags/6.2.2@739214 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/head.php
@@ -0,0 +1,161 @@
+<?php
+/*
+License: GPLv3
+License URI: http://surniaulula.com/wp-content/plugins/nextgen-facebook/license/gpl.txt
+Copyright 2012-2013 - Jean-Sebastien Morisset - http://surniaulula.com/
+*/
+
+if ( ! defined( 'ABSPATH' ) ) 
+	die( 'Sorry, you cannot call this webpage directly.' );
+
+if ( ! class_exists( 'ngfbHead' ) ) {
+
+	class ngfbHead {
+
+		public $og;
+
+		private $ngfb;
+
+		public function __construct( &$ngfb_plugin ) {
+			$this->ngfb =& $ngfb_plugin;
+			$this->ngfb->debug->mark();
+			$this->og = new ngfbOpenGraph( $ngfb_plugin );
+
+			add_action( 'wp_head', array( &$this, 'add_header' ), NGFB_HEAD_PRIORITY );
+		}
+
+		// called by WP wp_head action
+		public function add_header() {
+
+			if ( $this->ngfb->debug->is_on() ) {
+				$this->ngfb->debug->log( 'is_archive() = ' . ( is_archive() ? 'true' : 'false' ) );
+				$this->ngfb->debug->log( 'is_attachment() = ' . ( is_attachment() ? 'true' : 'false' ) );
+				$this->ngfb->debug->log( 'is_category() = ' . ( is_category() ? 'true' : 'false' ) );
+				$this->ngfb->debug->log( 'is_home() = ' . ( is_home() ? 'true' : 'false' ) );
+				$this->ngfb->debug->log( 'is_search() = ' . ( is_search() ? 'true' : 'false' ) );
+				$this->ngfb->debug->log( 'is_singular() = ' . ( is_singular() ? 'true' : 'false' ) );
+			}
+
+			$this->html( $this->og->get() );
+
+			if ( $this->ngfb->debug->is_on() ) {
+				//$defined_constants = get_defined_constants( true );
+				//$this->ngfb->debug->show_html( $this->ngfb->util->preg_grep_keys( '/^NGFB_/', $defined_constants['user'] ), 'NGFB Constants' );
+
+				$opts = $this->ngfb->options;
+				foreach ( array( 'ngfb_pro_tid', 'ngfb_googl_api_key', ) as $key ) $opts[$key] = '********';
+
+				$this->ngfb->debug->show_html( $this->ngfb->is_avail, 'Available Features' );
+				$this->ngfb->debug->show_html( null, 'Debug Log' );
+				$this->ngfb->debug->show_html( $opts, 'NGFB Settings' );
+			}
+		}
+
+		// called from the work/header.php template
+		public function html( $html_tags = array() ) {
+			global $post;
+			$author_url = '';
+		
+			echo "\n<!-- ", $this->ngfb->fullname, " meta tags BEGIN -->\n";
+
+			// show the array structure before the html block
+			$this->ngfb->debug->show_html( print_r( $html_tags, true ), 'Open Graph Array' );
+			$this->ngfb->debug->show_html( print_r( $this->ngfb->util->get_urls_found(), true ), 'URLs Found' );
+
+			echo '<meta name="generator" content="', $this->ngfb->fullname, ' ', $this->ngfb->version, '" />', "\n";
+
+			/*
+			 * Meta Tags for Google
+			 */
+			if ( ! empty( $html_tags['link:publisher'] ) )
+				echo '<link rel="publisher" href="', $html_tags['link:publisher'], '" />', "\n";
+			elseif ( ! empty( $this->ngfb->options['link_publisher_url'] ) )
+				echo '<link rel="publisher" href="', $this->ngfb->options['link_publisher_url'], '" />', "\n";
+
+			if ( ! empty( $html_tags['link:author'] ) )
+				echo '<link rel="author" href="', $html_tags['link:author'], '" />', "\n";
+			else {
+				if ( is_singular() ) {
+
+					if ( ! empty( $post ) && $post->post_author )
+						$author_url = $this->ngfb->user->get_author_url( $post->post_author, 
+							$this->ngfb->options['link_author_field'] );
+
+					elseif ( ! empty( $this->ngfb->options['link_def_author_id'] ) )
+						$author_url = $this->ngfb->user->get_author_url( $this->ngfb->options['link_def_author_id'], 
+							$this->ngfb->options['link_author_field'] );
+
+				// check for default author info on indexes and searches
+				} elseif ( ( ! is_singular() && ! is_search() && ! empty( $this->ngfb->options['link_def_author_on_index'] ) && ! empty( $this->ngfb->options['link_def_author_id'] ) )
+					|| ( is_search() && ! empty( $this->ngfb->options['link_def_author_on_search'] ) && ! empty( $this->ngfb->options['link_def_author_id'] ) ) ) {
+
+					$author_url = $this->ngfb->user->get_author_url( $this->ngfb->options['link_def_author_id'], 
+						$this->ngfb->options['link_author_field'] );
+				}
+				if ( $author_url ) 
+					echo '<link rel="author" href="', $author_url, '" />', "\n";
+			}
+
+			if ( ! empty( $this->ngfb->options['inc_description'] ) ) {
+				if ( is_singular() && ! empty( $post ) )
+					$link_desc = $this->ngfb->meta->get_options( $post->ID, 'link_desc' );
+				if ( empty( $link_desc ) )
+					$link_desc = $this->ngfb->webpage->get_description( $this->ngfb->options['link_desc_len'], '...' );
+				if ( ! empty( $link_desc ) ) 
+					echo '<meta name="description" content="', $link_desc, '" />', "\n";
+			}
+
+			/*
+			 * Print the Multi-Dimensional Array as HTML
+			 */
+			$this->ngfb->debug->log( count( $html_tags ) . ' html_tags to process' );
+			foreach ( $html_tags as $first_name => $first_val ) {					// 1st-dimension array (associative)
+				if ( is_array( $first_val ) ) {
+					//$this->ngfb->debug->log( 'foreach 1st-dimension element: ' . $first_name . ' (array)' );
+					foreach ( $first_val as $second_num => $second_val ) {			// 2nd-dimension array
+						if ( $this->ngfb->util->is_assoc( $second_val ) ) {
+							//$this->ngfb->debug->log( 'foreach 2nd-dimension element: ' . $second_num . ' (array)' );
+							ksort( $second_val );
+							foreach ( $second_val as $third_name => $third_val ) {	// 3rd-dimension array (associative)
+								//$this->ngfb->debug->log( 'formatting 3rd-dimension element: ' . $third_name );
+								echo $this->get_meta_html( $third_name, $third_val, $first_name . ':' . ( $second_num + 1 ) );
+							}
+							unset ( $third_name, $third_val );
+						} else {
+							//$this->ngfb->debug->log( 'formatting 2nd-dimension element: ' . $second_num );
+							echo $this->get_meta_html( $first_name, $second_val, $first_name . ':' . ( $second_num + 1 ) );
+						}
+					}
+					unset ( $second_num, $second_val );
+				} else {
+					//$this->ngfb->debug->log( 'formatting 1st-dimension element: ' . $first_name );
+					echo $this->get_meta_html( $first_name, $first_val );
+				}
+			}
+			unset ( $first_name, $first_val );
+
+			echo "<!-- ", $this->ngfb->fullname, " meta tags END -->\n";
+		}
+
+		private function get_meta_html( $name, $val = '', $cmt = '' ) {
+			$meta_html = '';
+			if ( ! empty( $this->ngfb->options['inc_' . $name] ) ) {
+				if ( ! empty( $val ) || ( ! empty( $this->ngfb->options['og_empty_tags'] ) && strpos( $name, 'og:' ) === 0 ) ) {
+					$charset = get_bloginfo( 'charset' );
+					$val = htmlentities( $this->ngfb->util->cleanup_html_tags( $this->ngfb->util->decode( $val ) ), 
+						ENT_QUOTES, $charset, false );
+					$this->ngfb->debug->log( 'meta ' . $name . ' = "' . $val . '"' );
+					if ( $cmt ) $meta_html .= "<!-- $cmt -->";
+					if ( strpos( $name, 'twitter:' ) === 0 )
+						$meta_html .= '<meta name="' . $name . '" content="' . $val . '" />' . "\n";
+					else
+						$meta_html .= '<meta property="' . $name . '" content="' . $val . '" />' . "\n";
+				} else $this->ngfb->debug->log( 'meta ' . $name . ' is empty - skipping' );
+			} else $this->ngfb->debug->log( 'meta ' . $name . ' is disabled - skipping' );
+			return $meta_html;
+		}
+
+	}
+
+}
+?>
---
+++ lib/util.php
@@ -0,0 +1,457 @@
+<?php
+/*
+License: GPLv3
+License URI: http://surniaulula.com/wp-content/plugins/nextgen-facebook/license/gpl.txt
+Copyright 2012-2013 - Jean-Sebastien Morisset - http://surniaulula.com/
+*/
+
+if ( ! defined( 'ABSPATH' ) ) 
+	die( 'Sorry, you cannot call this webpage directly.' );
+
+if ( ! class_exists( 'ngfbUtil' ) ) {
+
+	class ngfbUtil {
+
+		public $rewrite;
+
+		protected $ngfb;
+
+		private $goo;	// ngfbGoogl
+		private $urls_found = array();
+
+		// executed by ngfbUtilPro() as well
+		public function __construct( &$ngfb_plugin ) {
+			$this->ngfb =& $ngfb_plugin;
+			$this->ngfb->debug->mark();
+			$this->setup_vars();
+			$this->add_actions();
+		}
+
+		private function setup_vars() {
+			if ( class_exists( 'ngfbGoogl' ) ) {
+				$api_key = empty( $this->ngfb->options['ngfb_googl_api_key'] ) ? 
+					'' : $this->ngfb->options['ngfb_googl_api_key'];
+				$this->goo = new ngfbGoogl( $api_key, $this->ngfb->debug );
+			}
+		}
+
+		protected function add_actions() {
+			add_action( 'wp_scheduled_delete', array( &$this, 'delete_expired_transients' ) );
+			add_action( 'wp_scheduled_delete', array( &$this, 'delete_expired_cache' ) );
+		}
+
+		public function is_assoc( $arr ) {
+			if ( ! is_array( $arr ) ) return false;
+			return is_numeric( implode( array_keys( $arr ) ) ) ? false : true;
+		}
+
+		public function preg_grep_keys( $preg, $arr, $invert = false, $trunc = false, $rep = '' ) {
+			if ( ! is_array( $arr ) ) return false;
+			$invert = $invert == false ? null : PREG_GREP_INVERT;
+			$match = preg_grep( $preg, array_keys( $arr ), $invert );
+			$found = array();
+			foreach ( $match as $key ) {
+				if ( $trunc == true ) {
+					$fixed = preg_replace( $preg, $rep, $key );
+					$found[$fixed] = $arr[$key]; 
+				} else $found[$key] = $arr[$key]; 
+			}
+			return $found;
+		}
+
+		public function get_urls_found() {
+			return $this->urls_found;
+		}
+
+		public function is_uniq_url( $url = '' ) {
+
+			if ( empty( $url ) ) return false;
+
+			if ( ! preg_match( '/[a-z]+:\/\//i', $url ) )
+				$this->ngfb->debug->log( 'incomplete url given: ' . $url );
+
+			if ( empty( $this->urls_found[$url] ) ) {
+				$this->urls_found[$url] = 1;
+				return true;
+			} else {
+				$this->ngfb->debug->log( 'duplicate image rejected: ' . $url ); 
+				return false;
+			}
+		}
+
+		// $use_post = false when used for Open Graph meta tags and buttons in widget
+		// $use_post = true when buttons are added to individual posts on an index webpage
+		public function get_sharing_url( $strip_query = 'notrack', $url = '', $use_post = false ) {
+			if ( ! empty( $url ) )  {
+				$url = $this->fix_relative_url( $url );
+			} else {
+				global $post;
+				$is_nggalbum = false;
+
+				// check for album/gallery query strings and an [nggalbum] shortcode
+				if ( is_singular() ) {
+					global $wp_query;
+					// sanitize query values
+					$ngg_album = empty( $wp_query->query['album'] ) ? '' : preg_replace( '/[^0-9]/', '', $wp_query->query['album'] );
+					$ngg_gallery = empty( $wp_query->query['gallery'] ) ? '' : preg_replace( '/[^0-9]/', '', $wp_query->query['gallery'] );
+
+					if ( ( ! empty( $ngg_album ) || ! empty( $ngg_gallery ) ) && ! empty( $post ) && 
+						preg_match( '/\[(nggalbum|album)(| [^\]]*id=[\'"]*([0-9]+)[\'"]*[^\]]*| [^\]]*)\]/im', $post->post_content ) ) {
+						$this->ngfb->debug->log( 'is_singular with nggalbum shortcode and query' );
+						$is_nggalbum = true;
+						$strip_query = 'notrack';	// keep the album/gallery query values
+					}
+				}
+
+				// use permalink for singular pages (without nggalbum query info) or posts within a loop (use_post is true)
+				if ( ( is_singular() && $is_nggalbum == false ) || ( $use_post && ! empty( $post ) ) ) {
+					$url = get_permalink( $post->ID );
+					$strip_query = 'none';	// don't modify the permalinks
+				} else {
+					$url = empty( $_SERVER['HTTPS'] ) ? 'http://' : 'https://';
+					$url .= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+				}
+			}
+			switch ( $strip_query ) {
+				case 'noquery' :
+					if ( strpos( $url, '?' ) !== false ) 
+						$url = reset( explode( '?', $url ) );
+					break;
+				case 'notrack' :
+					// strip out tracking query arguments by facebook, google, etc.
+					$url = preg_replace( '/([\?&])(fb_action_ids|fb_action_types|fb_source|fb_aggregation_id|utm_source|utm_medium|utm_campaign|utm_term|gclid|pk_campaign|pk_kwd)=[^&]*&?/i', '$1', $url );
+					break;
+				// leave url as-is
+				default :
+					break;
+			}
+			return $url;
+		}
+
+		public function get_cache_url( $url ) {
+
+			// make sure the cache expiration is greater than 0 hours
+			if ( empty( $this->ngfb->cache->file_expire ) ) return $url;
+
+			// facebook javascript sdk doesn't work when hosted locally
+			if ( preg_match( '/connect.facebook.net/', $url ) ) return $url;
+
+			return ( $this->ngfb->util->rewrite( $this->ngfb->cache->get( $url ) ) );
+		}
+
+		public function get_short_url( $url, $shorten = true ) {
+
+			// return original URL if curl not installed or disabled
+			// $shorten can be 'true' or '1', so test with empty()
+			if ( ! function_exists( 'curl_init' ) || ! class_exists( 'ngfbGoogl' ) ||
+				empty( $shorten ) || ( defined( 'NGFB_CURL_DISABLE' ) && NGFB_CURL_DISABLE ) ) 
+					return $url;
+
+			$cache_salt = __METHOD__ . '(url:' . $url . ')';
+			$cache_id = $this->ngfb->acronym . '_' . md5( $cache_salt );
+			$cache_type = 'object cache';
+			$short_url = get_transient( $cache_id );
+			$this->ngfb->debug->log( $cache_type . ': short_url transient id salt "' . $cache_salt . '"' );
+
+			if ( $short_url !== false ) {
+				$this->ngfb->debug->log( $cache_type . ': short_url retrieved from transient for id "' . $cache_id . '"' );
+				$url = $short_url;
+			} else {
+				$short_url = $this->goo->shorten( $url );
+				if ( empty( $short_url ) )
+					$this->ngfb->debug->log( 'failed to shorten url = ' . $url );
+				else {
+					$this->ngfb->debug->log( 'url successfully shortened = ' . $short_url );
+					set_transient( $cache_id, $short_url, $this->ngfb->cache->object_expire );
+					$this->ngfb->debug->log( $cache_type . ': short_url saved to transient for id "' . 
+						$cache_id . '" (' . $this->ngfb->cache->object_expire . ' seconds)' );
+					$url = $short_url;
+				}
+			}
+			return $url;
+		}
+
+		public function fix_relative_url( $url = '' ) {
+			if ( ! empty( $url ) && ! preg_match( '/[a-z]+:\/\//i', $url ) ) {
+				$this->ngfb->debug->log( 'relative url found = ' . $url );
+				// if it starts with a slash, just add the home_url() prefix
+				if ( preg_match( '/^\//', $url ) ) $url = home_url( $url );
+				else $url = trailingslashit( $this->get_sharing_url( 'noquery' ), false ) . $url;
+				$this->ngfb->debug->log( 'relative url fixed = ' . $url );
+			}
+			return $url;
+		}
+	
+		public function decode( $str ) {
+			// if we don't have something to decode, return immediately
+			if ( strpos( $str, '&#' ) === false ) return $str;
+
+			// convert certain entities manually to something non-standard
+			$str = preg_replace( '/&#8230;/', '...', $str );
+
+			// if mb_decode_numericentity is not available, return the string un-converted
+			if ( $this->ngfb->is_avail['mbdecnum'] != true ) return $str;
+
+			return preg_replace( '/&#\d{2,5};/ue', 'ngfbUtil::decode_utf8_entity( \'$0\' )', $str );
+		}
+
+		private function decode_utf8_entity( $entity ) {
+			$convmap = array( 0x0, 0x10000, 0, 0xfffff );
+			return mb_decode_numericentity( $entity, $convmap, 'UTF-8' );
+		}
+
+		public function limit_text_length( $text, $textlen = 300, $trailing = '' ) {
+			$text = preg_replace( '/<\/p>/i', ' ', $text);				// replace end of paragraph with a space
+			$text = $this->cleanup_html_tags( $text );				// remove any remaining html tags
+			if ( strlen( $trailing ) > $textlen )
+				$trailing = substr( $text, 0, $textlen );			// trim the trailing string, if too long
+			if ( strlen( $text ) > $textlen ) {
+				$text = substr( $text, 0, $textlen - strlen( $trailing ) );
+				$text = trim( preg_replace( '/[^ ]*$/', '', $text ) );		// remove trailing bits of words
+				$text = preg_replace( '/[,\.]*$/', '', $text );			// remove trailing puntuation
+			} else $trailing = '';							// truncate trailing string if text is shorter than limit
+			$text = esc_attr( $text ) . $trailing;					// trim and add trailing string (if provided)
+			return $text;
+		}
+
+		public function cleanup_html_tags( $text, $strip_tags = true ) {
+			$text = strip_shortcodes( $text );							// remove any remaining shortcodes
+			$text = preg_replace( '/[\r\n\t ]+/s', ' ', $text );					// put everything on one line
+			$text = preg_replace( '/<\?.*\?>/i', ' ', $text);					// remove php
+			$text = preg_replace( '/<script\b[^>]*>(.*?)<\/script>/i', ' ', $text);			// remove javascript
+			$text = preg_replace( '/<style\b[^>]*>(.*?)<\/style>/i', ' ', $text);			// remove inline stylesheets
+			$text = preg_replace( '/<!--ngfb-ignore-->(.*?)<!--\/ngfb-ignore-->/i', ' ', $text);	// remove text between comment strings
+			if ( $strip_tags == true ) $text = strip_tags( $text );					// remove remaining html tags
+			return trim( $text );
+		}
+
+		public function rewrite( $url = '' ) {
+			if ( $this->ngfb->is_avail['aop'] == true ) {
+				$url = '"' . $url . '"';	// rewrite function uses var reference
+				$url = trim( $this->rewrite->html( $url ), '"' );
+			}
+			return $url;
+		}
+
+		public function get_topics() {
+			// list from http://en.wikipedia.org/wiki/Category:Websites_by_topic
+			$website_topics = array(
+				'Animation',
+				'Architecture',
+				'Art',
+				'Automotive',
+				'Aviation',
+				'Chat',
+				'Children\'s',
+				'Comics',
+				'Commerce',
+				'Community',
+				'Dance',
+				'Dating',
+				'Digital Media',
+				'Documentary',
+				'Download',
+				'Economics',
+				'Educational',
+				'Employment',
+				'Entertainment',
+				'Environmental',
+				'Erotica and Pornography',
+				'Fashion',
+				'File Sharing',
+				'Food and Drink',
+				'Fundraising',
+				'Genealogy',
+				'Health',
+				'History',
+				'Humor',
+				'Law Enforcement',
+				'Legal',
+				'Literature',
+				'Medical',
+				'Military',
+				'News',
+				'Nostalgia',
+				'Parenting',
+				'Photography',
+				'Political',
+				'Religious',
+				'Review',
+				'Reward',
+				'Route Planning',
+				'Satirical',
+				'Science Fiction',
+				'Science',
+				'Shock',
+				'Social Networking',
+				'Spiritual',
+				'Sport',
+				'Technology',
+				'Travel',
+				'Vegetarian',
+				'Webmail',
+				'Women\'s',
+			);
+			if ( $this->ngfb->is_avail['aop'] ) 
+				return apply_filters( 'ngfb_topics', $website_topics );			// since wp 0.71 
+			natsort( $website_topics );
+			// after sorting the array, put 'none' first
+			$website_topics = array_merge( array( 'none' ), $website_topics );
+			return $website_topics;
+		}
+
+		public function parse_readme( $url, $expire_secs = false ) {
+			$cache_salt = __METHOD__ . '(file:' . $this->ngfb->urls['readme'] . ')';
+			$cache_id = $this->ngfb->acronym . '_' . md5( $cache_salt );
+			$cache_type = 'object cache';
+			$plugin_info = get_transient( $cache_id );
+			$this->ngfb->debug->log( $cache_type . ': plugin_info transient id salt "' . $cache_salt . '"' );
+
+			if ( $plugin_info !== false ) {
+				$this->ngfb->debug->log( $cache_type . ': plugin_info retrieved from transient for id "' . $cache_id . '"' );
+				return $plugin_info;
+			}
+
+			$using_local = false;
+			$readme = $this->ngfb->cache->get( $this->ngfb->urls['readme'], 'raw', 'file', $expire_secs );
+			// fallback to local readme.txt file
+			if ( empty( $readme ) && $fh = @fopen( NGFB_PLUGINDIR . 'readme.txt', 'rb' ) ) {
+				$using_local = true;
+				$readme = fread( $fh, filesize( NGFB_PLUGINDIR . 'readme.txt' ) );
+				fclose( $fh );
+			}
+			if ( ! empty( $readme ) ) {
+				$parser = new ngfb_parse_readme( $this->ngfb->debug );
+				$plugin_info = $parser->parse_readme_contents( $readme );
+				if ( $using_local == true ) {
+					foreach ( array( 'stable_tag', 'upgrade_notice' ) as $key )
+						if ( array_key_exists( $key, $plugin_info ) )
+							unset( $plugin_info[$key] );
+				}
+			}
+
+			set_transient( $cache_id, $plugin_info, $this->ngfb->cache->object_expire );
+			$this->ngfb->debug->log( $cache_type . ': plugin_info saved to transient for id "' . $cache_id . '" (' . $this->ngfb->cache->object_expire . ' seconds)');
+			return $plugin_info;
+		}
+
+		public function get_admin_url( $submenu = '', $link_text = '' ) {
+			$query = '';
+			if ( strpos( $submenu, '?' ) !== false )
+				list( $submenu, $query ) = explode( '?', $submenu );
+			if ( $submenu == '' ) {
+				$current = $_SERVER['REQUEST_URI'];
+				if ( preg_match( '/^.*\?page=' . $this->ngfb->acronym . '-([^&]*).*$/', $current, $match ) )
+					$submenu = $match[1];
+				else $submenu = 'general';
+			} else {
+				if ( ! array_key_exists( $submenu, $this->ngfb->setting_libs ) )
+					$submenu = 'general';
+			}
+			$url = get_admin_url( null, 'admin.php?page=' . $this->ngfb->acronym . '-' . $submenu );
+			if ( ! empty( $query ) ) $url .= '&' . $query;
+			if ( empty( $link_text ) ) return $url;
+			else return '<a href="' . $url . '">' . $link_text . '</a>';
+		}
+
+		public function delete_expired_transients( $clear_all = false ) { 
+			global $wpdb, $_wp_using_ext_object_cache;
+			$deleted = 0;
+			if ( $_wp_using_ext_object_cache ) return; 
+			$time = isset ( $_SERVER['REQUEST_TIME'] ) ? (int) $_SERVER['REQUEST_TIME'] : time() ; 
+		
+			$dbquery = 'SELECT option_name FROM ' . $wpdb->options . ' WHERE option_name LIKE \'_transient_timeout_' . $this->ngfb->acronym . '_%\'';
+			$dbquery .= $clear_all === true ? ';' : ' AND option_value < ' . $time . ';'; 
+			$expired = $wpdb->get_col( $dbquery ); 
+			
+			foreach( $expired as $transient ) { 
+				$key = str_replace('_transient_timeout_', '', $transient);
+				delete_transient( $key );
+				$deleted++;
+			}
+			return $deleted;
+		}
+
+		public function delete_expired_cache( $clear_all = false ) {
+			$deleted = 0;
+			if ( $dh = opendir( NGFB_CACHEDIR ) ) {
+				while ( $fn = readdir( $dh ) ) {
+					if ( ! preg_match( '/^(\.|index\.php)/', $fn ) && is_file( NGFB_CACHEDIR . $fn ) && 
+						( $clear_all === true || filemtime( NGFB_CACHEDIR . $fn ) < time() - $this->ngfb->cache->file_expire ) ) {
+
+						unlink( NGFB_CACHEDIR . $fn );
+						$deleted++;
+					}
+				}
+				closedir( $dh );
+			}
+			return $deleted;
+		}
+
+		public function push_max( &$dst, &$src, $num = 0 ) {
+			if ( ! is_array( $dst ) || ! is_array( $src ) ) return false;
+			if ( ! empty( $src ) ) array_push( $dst, $src );
+			return $this->slice_max( $dst, $num );
+		}
+
+		public function slice_max( &$arr, $num = 0 ) {
+			if ( ! is_array( $arr ) ) return false;
+			$has = count( $arr );
+			if ( $num > 0 ) {
+				if ( $has == $num ) {
+					$this->ngfb->debug->log( 'max values reached (' . $has . ' == ' . $num . ')' );
+					return true;
+				} elseif ( $has > $num ) {
+					$this->ngfb->debug->log( 'max values reached (' . $has . ' > ' . $num . ') - slicing array' );
+					$arr = array_slice( $arr, 0, $num );
+					return true;
+				}
+			}
+			return false;
+		}
+
+		// table header with optional tooltip text
+		public function th( $title = '', $class = '', $id = '', $tooltip = '' ) {
+			$html = '<th'.
+				( empty( $class ) ? '' : ' class="'.$class.'"' ) .
+				( empty( $id ) ? '' : ' id="'.$id.'"' ) . 
+				'><p>' .  $title;
+			if ( ! empty( $tooltip ) )
+				$html .= '<img src="' . NGFB_URLPATH . 'images/question-mark.png" 
+					class="ngfb_tooltip" alt="' . esc_attr( $tooltip ) . '" />';
+			$html .= '</p></th>' . "\n";
+			return $html;
+		}
+
+		public function do_tabs( $prefix = '', $tabs = array(), $tab_rows = array(), $scroll_to = '' ) {
+			$default_tab = reset( array_keys( $tabs ) );
+			$prefix = empty( $prefix ) ? '' : '_' . $prefix;
+			$class_tabs = 'ngfb-metabox-tabs' . ( empty( $prefix ) ? '' : ' ngfb-metabox-tabs' . $prefix );
+			$class_link = 'ngfb-tablink' . ( empty( $prefix ) ? '' : ' ngfb-tablink' . $prefix );
+			$class_tab = 'ngfb-tab';
+			echo '<script type="text/javascript">jQuery(document).ready(function(){ 
+				ngfbTabs(\'', $prefix, '\', \'', $default_tab, '\', \'', $scroll_to, '\'); });</script>
+			<div class="', $class_tabs, '">
+			<ul class="', $class_tabs, '">';
+			foreach ( $tabs as $key => $title ) {
+				$href_key = $class_tab . $prefix . '_' . $key;
+				echo '<li class="', $href_key, '"><a class="', $class_link, '" href="#', $href_key, '">', $title, '</a></li>';
+			}
+			echo '</ul>';
+			foreach ( $tabs as $key => $title ) {
+				$href_key = $class_tab . $prefix . '_' . $key;
+				echo '<div class="', $class_tab, ( empty( $prefix ) ? '' : ' ' . $class_tab . $prefix ), ' ', $href_key, '">';
+				echo '<table class="ngfb-settings">';
+				if ( ! empty( $tab_rows[$key] ) && is_array( $tab_rows[$key] ) )
+					foreach ( $tab_rows[$key] as $row ) 
+						echo '<tr>' . $row . '</tr>';
+				echo '</table>';
+				echo '</div>';
+			}
+			echo '</div>';
+		}
+
+	}
+
+}
+?>
