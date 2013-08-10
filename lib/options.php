b21ccef8aee1e5156f8f8e8772f8c06945e3fb31
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-10 10:23:44 +0000

    fixed twiiter username - allowed chars
    git-svn-id: https://plugins.svn.wordpress.org/nextgen-facebook/tags/6.6@754439 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/options.php
+++ lib/options.php
@@ -336,7 +336,7 @@ if ( ! class_exists( 'ngfbOptions' ) ) {
 
 						// twitter-style usernames (a-z0-9, max 15 chars)
 						case 'tc_site' :
-							$opts[$key] = substr( preg_replace( '/[^a-z0-9]/', '', 
+							$opts[$key] = substr( preg_replace( '/[^a-z0-9_]/', '', 
 								strtolower( $opts[$key] ) ), 0, 15 );
 							if ( ! empty( $opts[$key] ) ) 
 								$opts[$key] = '@' . $opts[$key];
