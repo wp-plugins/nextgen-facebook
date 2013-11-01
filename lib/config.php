16fca7c1e176de9517372af7539b4bd491bd3daa
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-01 14:48:10 +0000

    readme update
    git-svn-id: https://plugins.svn.wordpress.org/nextgen-facebook/trunk@797275 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/config.php
+++ lib/config.php
@@ -13,7 +13,7 @@ if ( ! class_exists( 'ngfbPluginConfig' ) ) {
 	class ngfbPluginConfig {
 
 		private static $cf = array(
-			'version' => '6.14rc1',			// plugin version
+			'version' => '6.14.0',			// plugin version
 			'lca' => 'ngfb',			// lowercase acronym
 			'uca' => 'NGFB',			// uppercase acronym
 			'slug' => 'nextgen-facebook',
