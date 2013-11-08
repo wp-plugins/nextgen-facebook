c7e615977b84f670428dd3792f366d5a26fe908c
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-08 14:09:22 +0000

    new tag 6.15dev4
    git-svn-id: https://plugins.svn.wordpress.org/nextgen-facebook/tags/6.15dev4@801112 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/messages.php
@@ -0,0 +1,91 @@
+<?php
+/*
+License: GPLv3
+License URI: http://surniaulula.com/wp-content/plugins/nextgen-facebook/license/gpl.txt
+Copyright 2012-2013 - Jean-Sebastien Morisset - http://surniaulula.com/
+*/
+
+if ( ! defined( 'ABSPATH' ) ) 
+	die( 'These aren\'t the droids you\'re looking for...' );
+
+if ( ! class_exists( 'NgfbMessages' ) ) {
+
+	class NgfbMessages {
+
+		protected $p;
+
+		public function __construct( &$plugin ) {
+			$this->p =& $plugin;
+			$this->p->debug->mark();
+		}
+
+		public function get( $name = '' ) {
+			$msg = '';
+			switch ( $name ) {
+				case 'pro_feature' :
+					if ( $this->p->is_avail['aop'] == true ) {
+						$msg = '<p class="pro_feature"><a href="'.$this->p->cf['url']['purchase'].'" target="_blank">Purchase 
+						additional licence(s) to enable Pro version features</p>';
+					} else
+						$msg = '<p class="pro_feature"><a href="'.$this->p->cf['url']['purchase'].'" target="_blank">Upgrade 
+						to the Pro version to enable the following features</a></p>';
+					break;
+				case 'pro_activate' :
+					// in multisite, only show activation message on our own plugin pages
+					if ( ! is_multisite() || ( is_multisite() && preg_match( '/^.*\?page='.$this->p->cf['lca'].'-/', $_SERVER['REQUEST_URI'] ) ) ) {
+						$url = $this->p->util->get_admin_url( 'advanced' );
+						$msg = '<p>The '.$this->p->cf['full'].' Authentication ID option value is empty.<br/>
+						To activate Pro version features, and allow the plugin to authenticate itself for updates,<br/>
+						<a href="'.$url.'">enter the unique Authenticaton ID you receive following your purchase
+						on the Advanced Settings page</a>.</p>';
+					}
+					break;
+				case 'pro_details' :
+					$msg = '<p style="font-weight:bold;font-size:1.1em;">Would you like to... 
+					Add support for <a href="https://dev.twitter.com/docs/cards" target="_blank">Twitter Cards</a>,
+					including <em>Gallery, Photo, Large Image, Player and Product</em> Cards?<br/>';
+					$msg .= 'Customize Open Graph and Twitter Card meta tags for each <em>individual</em> Post and Page?<br/>';
+					$msg .= 'Change the Facebook, Google+ and Twitter social button language as the webpage switches language?<br/>';
+					$msg .= 'Add tighter integration with 3rd party plugins like WordPress SEO, All-In-One SEO and WooCommerce?<br/>';
+					$msg .= 'Improve page load times with file caching for <em>external</em> social images and JavaScript?<br/>';
+					$msg .= '<p style="font-size:1.2em;">Help support '.$this->p->cf['full'].' by <a href="'.$this->p->cf['url']['purchase'].'" 
+					target="_blank">purchasing the Pro version today</a>.</p>';
+					$msg .= '<p>Upgrading to the Pro version is easy and simple! Enter the unique <em>Authentication ID</em> 
+					(that you\'ll receive by email) on the Advanced settings page, and update the plugin from within WordPress.</p>';
+					break;
+				case 'purchase_box' :
+					$msg = '<p>Developing and supporting the '.$this->p->cf['full'].' plugin takes most of my work days (and week-ends).
+					If you compare this plugin with others, I hope you\'ll agree that the result was worth all the effort and long hours.
+					<a href="'.$this->p->cf['url']['purchase'].'" target="_blank">If you would like to show your appreciation, and
+					access the full range of features this plugin has to offer, please purchase ';
+					if ( $this->p->is_avail['aop'] == true )
+						$msg .= 'a Pro version license</a>.</p>';
+					else $msg .= 'the Pro version</a>.</p>';
+					break;
+				case 'thankyou' :
+					$msg = '<p>Thank you for your purchase! I hope the '.$this->p->cf['full'].' plugin will exceed all of your expectations.</p>';
+					break;
+				case 'help_boxes' :
+					$msg = '<p>Individual option boxes (like this one) can be opened / closed by clicking on their title bar, 
+					moved and re-ordered by dragging them, and removed / added from the <em>Screen Options</em> tab (top-right).
+					Values in multiple tabs can be edited before clicking the \'Save All Changes\' button.</p>';
+					break;
+				case 'help_free' :
+					$msg = '<p><strong>Need help with the <em>GPL</em> version? 
+					See the <a href="'.$this->p->cf['url']['faq'].'" target="_blank">FAQ</a>, 
+					the <a href="'.$this->p->cf['url']['notes'].'" target="_blank">Other Notes</a>, or visit the 
+					<a href="'.$this->p->cf['url']['support'].'" target="_blank">Support Forum</a> on WordPress.org</strong>.</p>';
+					break;
+				case 'help_pro' :
+					$msg = '<p><strong>Need help with the Pro version? 
+					See the <a href="'.$this->p->cf['url']['pro_faq'].'" target="_blank">FAQ</a>, 
+					the <a href="'.$this->p->cf['url']['pro_notes'].'" target="_blank">Other Notes</a>, or 
+					submit a <a href="'.$this->p->cf['url']['pro_request'].'" target="_blank">Support Request</a>.</p>';
+					break;
+			}
+			return $msg;
+		}
+	}
+}
+
+?>
---
+++ lib/websites/stumbleupon.php
@@ -0,0 +1,120 @@
+<?php
+/*
+License: GPLv3
+License URI: http://surniaulula.com/wp-content/plugins/nextgen-facebook/license/gpl.txt
+Copyright 2012-2013 - Jean-Sebastien Morisset - http://surniaulula.com/
+*/
+
+if ( ! defined( 'ABSPATH' ) ) 
+	die( 'These aren\'t the droids you\'re looking for...' );
+
+if ( ! class_exists( 'NgfbAdminSocialStumbleupon' ) && class_exists( 'NgfbAdminSocial' ) ) {
+
+	class NgfbAdminSocialStumbleupon extends NgfbAdminSocial {
+
+		protected $p;
+
+		public function __construct( &$plugin ) {
+			$this->p =& $plugin;
+			$this->p->debug->mark();
+		}
+
+		public function get_rows() {
+			$prot = empty( $_SERVER['HTTPS'] ) ? 'http://' : 'https://';
+			$badge = '
+				<style type="text/css">
+					.badge { 
+						display:block;
+						background: url("'.$this->p->util->get_cache_url( 
+							$prot.'b9.sustatic.com/7ca234_0mUVfxHFR0NAk1g' ).'") no-repeat transparent; 
+						width:110px;
+						margin:5px 0 5px 0;
+					}
+					.badge input[type=radio] {
+					}
+					.badge-col-left { display:inline-block; float:left; margin-right:20px; }
+					.badge-col-right { display:inline-block; }
+					#badge-1 { height:20px; background-position:25px 0px; }
+					#badge-2 { height:20px; background-position:25px -100px; }
+					#badge-3 { height:20px; background-position:25px -200px; }
+					#badge-4 { height:60px; background-position:25px -300px; }
+					#badge-5 { height:30px; background-position:25px -400px; }
+					#badge-6 { height:20px; background-position:25px -500px; }
+				</style>
+			';
+
+			$badge .= '<div class="badge-col-left">';
+			foreach ( array( 1, 2, 3, 6 ) as $i ) {
+				$badge .= '<div class="badge" id="badge-'.$i.'">';
+				$badge .= '<input type="radio" name="'.$this->p->admin->form->options_name.'[stumble_badge]" 
+					value="'.$i.'" '.checked( $i, $this->p->options['stumble_badge'], false ).'/>';
+				$badge .= '</div>';
+			}
+			$badge .= '</div><div class="badge-col-right">';
+			foreach ( array( 4, 5 ) as $i ) {
+				$badge .= '<div class="badge" id="badge-'.$i.'">';
+				$badge .= '<input type="radio" name="'.$this->p->admin->form->options_name.'[stumble_badge]" 
+					value="'.$i.'" '.checked( $i, $this->p->options['stumble_badge'], false ).'/>';
+				$badge .= '</div>';
+			}
+			$badge .= '</div>';
+
+			return array(
+				$this->p->util->th( 'Show Button in', 'short' ).'<td>'.
+				'Content '.$this->p->admin->form->get_checkbox( 'stumble_on_the_content' ).'&nbsp;'.
+				'Excerpt '.$this->p->admin->form->get_checkbox( 'stumble_on_the_excerpt' ).'&nbsp;'.
+				'Edit Post/Page '.$this->p->admin->form->get_checkbox( 'stumble_on_admin_sharing' ). 
+				'</td>',
+
+				$this->p->util->th( 'Preferred Order', 'short' ).'<td>'.
+				$this->p->admin->form->get_select( 'stumble_order', 
+					range( 1, count( $this->p->admin->settings['social']->website ) ), 'short' ).'</td>',
+
+				$this->p->util->th( 'JavaScript in', 'short' ).'<td>'.
+				$this->p->admin->form->get_select( 'stumble_js_loc', $this->js_locations ).'</td>',
+
+				$this->p->util->th( 'Button Style', 'short' ).'<td>'.$badge.'</td>',
+			);
+		}
+
+	}
+}
+
+if ( ! class_exists( 'NgfbSocialStumbleupon' ) && class_exists( 'NgfbSocial' ) ) {
+
+	class NgfbSocialStumbleupon {
+
+		protected $p;
+
+		public function __construct( &$plugin ) {
+			$this->p =& $plugin;
+			$this->p->debug->mark();
+		}
+
+		public function get_html( $atts = array(), $opts = array() ) {
+			$this->p->debug->mark();
+			if ( empty( $opts ) ) $opts = $this->p->options;
+			$html = '';
+			$use_post = empty( $atts['is_widget'] ) || is_singular() ? true : false;
+			$src_id = $this->p->util->get_src_id( 'stumbleupon', $atts );
+			$atts['url'] = empty( $atts['url'] ) ? 
+				$this->p->util->get_sharing_url( 'notrack', null, $use_post, $src_id ) : 
+				$this->p->util->get_sharing_url( 'asis', $atts['url'], null, $src_id );
+			if ( empty( $atts['stumble_badge'] ) ) $atts['stumble_badge'] = $opts['stumble_badge'];
+			$html = '<!-- StumbleUpon Button --><div '.$this->p->social->get_css( 'stumbleupon', $atts, 'stumble-button' ).'><su:badge layout="'.$atts['stumble_badge'].'" location="'.$atts['url'].'"></su:badge></div>';
+			$this->p->debug->log( 'returning html ('.strlen( $html ).' chars)' );
+			return $html;
+		}
+
+		public function get_js( $pos = 'id' ) {
+			$this->p->debug->mark();
+			$prot = empty( $_SERVER['HTTPS'] ) ? 'http://' : 'https://';
+			$js_url = $this->p->util->get_cache_url( $prot.'platform.stumbleupon.com/1/widgets.js' );
+
+			return '<script type="text/javascript" id="stumbleupon-script-'.$pos.'">'.$this->p->cf['lca'].'_insert_js( "stumbleupon-script-'.$pos.'", "'.$js_url.'" );</script>';
+		}
+
+	}
+
+}
+?>
