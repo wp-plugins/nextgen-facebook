a59eed6c17537e157f4fae1bf3614e5a7ce43c4f
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-09 13:59:58 +0000

    6.21.0
    git-svn-id: https://plugins.svn.wordpress.org/nextgen-facebook/tags/6.21.0@835466 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/widget/social.php
+++ lib/widget/social.php
@@ -2,7 +2,7 @@
 /*
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl.txt
-Copyright 2012-2013 - Jean-Sebastien Morisset - http://surniaulula.com/
+Copyright 2012-2014 - Jean-Sebastien Morisset - http://surniaulula.com/
 */
 
 if ( ! defined( 'ABSPATH' ) ) 
@@ -12,13 +12,18 @@ if ( ! class_exists( 'NgfbWidgetSocialSharing' ) && class_exists( 'WP_Widget' ) 
 
 	class NgfbWidgetSocialSharing extends WP_Widget {
 
+		protected $p;
+
 		public function __construct() {
 			global $ngfb;
-			$widget_name = $ngfb->cf['menu'].' Social Sharing';
-			$widget_class = $ngfb->cf['lca'].'-widget-buttons';
+			if ( ! is_object( $ngfb ) )
+				return;
+			$this->p =& $ngfb;
+			$widget_name = $this->p->cf['menu'].' Social Sharing';
+			$widget_class = $this->p->cf['lca'].'-widget-buttons';
 			$widget_ops = array( 
 				'classname' => $widget_class,
-				'description' => 'The '.$ngfb->cf['full'].' social sharing buttons widget.'
+				'description' => 'The '.$this->p->cf['full'].' social sharing buttons widget.'
 			);
 			$this->WP_Widget( $widget_class, $widget_name, $widget_ops );
 		}
@@ -26,24 +31,24 @@ if ( ! class_exists( 'NgfbWidgetSocialSharing' ) && class_exists( 'WP_Widget' ) 
 		public function widget( $args, $instance ) {
 			if ( is_feed() ) return;	// nothing to do in the feeds
 			if ( ! empty( $_SERVER['NGFB_DISABLE'] ) ) return;
+			if ( ! is_object( $this->p ) ) return;
 
-			global $ngfb;
-			if ( is_object( $ngfb->social ) && $ngfb->social->is_disabled() ) {
-				$ngfb->debug->log( 'widget buttons skipped: buttons disabled' );
+			if ( is_object( $this->p->social ) && $this->p->social->is_disabled() ) {
+				$this->p->debug->log( 'widget buttons skipped: buttons disabled' );
 				return;
 			}
 			extract( $args );
 
-			if ( $ngfb->is_avail['cache']['transient'] ) {
-				$sharing_url = $ngfb->util->get_sharing_url();
+			if ( $this->p->is_avail['cache']['transient'] ) {
+				$sharing_url = $this->p->util->get_sharing_url();
 				$cache_salt = __METHOD__.'(lang:'.get_locale().'_widget:'.$this->id.'_sharing_url:'.$sharing_url.')';
-				$cache_id = $ngfb->cf['lca'].'_'.md5( $cache_salt );
+				$cache_id = $this->p->cf['lca'].'_'.md5( $cache_salt );
 				$cache_type = 'object cache';
-				$ngfb->debug->log( $cache_type.': widget_html transient salt '.$cache_salt );
+				$this->p->debug->log( $cache_type.': widget_html transient salt '.$cache_salt );
 				$widget_html = get_transient( $cache_id );
 				if ( $widget_html !== false ) {
-					$ngfb->debug->log( $cache_type.': widget_html retrieved from transient '.$cache_id );
-					$ngfb->debug->show_html();
+					$this->p->debug->log( $cache_type.': widget_html retrieved from transient '.$cache_id );
+					$this->p->debug->show_html();
 					echo $widget_html;
 					return;
 				}
@@ -51,51 +56,49 @@ if ( ! class_exists( 'NgfbWidgetSocialSharing' ) && class_exists( 'WP_Widget' ) 
 
 			// sort enabled social buttons by their preferred order
 			$sorted_ids = array();
-			foreach ( $ngfb->cf['opt']['pre'] as $id => $pre )
+			foreach ( $this->p->cf['opt']['pre'] as $id => $pre )
 				if ( array_key_exists( $id, $instance ) && (int) $instance[$id] )
-					$sorted_ids[$ngfb->options[$pre.'_order'].'-'.$id] = $id;
+					$sorted_ids[$this->p->options[$pre.'_order'].'-'.$id] = $id;
 			unset ( $id, $pre );
 			ksort( $sorted_ids );
 
 			$atts = array( 'is_widget' => 1, 'css_id' => $args['widget_id'] );
 			$title = apply_filters( 'widget_title', $instance['title'], $instance, $this->id_base );
 
-			$widget_html = '<!-- '.$ngfb->cf['lca'].' '.$args['widget_id'].' begin -->';
+			$widget_html = '<!-- '.$this->p->cf['lca'].' '.$args['widget_id'].' begin -->';
 			$widget_html .= $before_widget;
 			if ( $title ) 
 				$widget_html .= $before_title.$title.$after_title;
-			$widget_html .= $ngfb->social->get_html( $sorted_ids, $atts );
+			$widget_html .= $this->p->social->get_html( $sorted_ids, $atts );
 			$widget_html .= $after_widget;
-			$widget_html .= '<!-- '.$ngfb->cf['lca'].' '.$args['widget_id'].' end -->';
+			$widget_html .= '<!-- '.$this->p->cf['lca'].' '.$args['widget_id'].' end -->';
 
-			if ( $ngfb->is_avail['cache']['transient'] ) {
-				set_transient( $cache_id, $widget_html, $ngfb->cache->object_expire );
-				$ngfb->debug->log( $cache_type.': widget_html saved to transient '.$cache_id.' ('.$ngfb->cache->object_expire.' seconds)');
+			if ( $this->p->is_avail['cache']['transient'] ) {
+				set_transient( $cache_id, $widget_html, $this->p->cache->object_expire );
+				$this->p->debug->log( $cache_type.': widget_html saved to transient '.$cache_id.' ('.$this->p->cache->object_expire.' seconds)');
 			}
-			$ngfb->debug->show_html();
+			$this->p->debug->show_html();
 			echo $widget_html;
 		}
 	
 		public function update( $new_instance, $old_instance ) {
-			global $ngfb;
 			$instance = $old_instance;
 			$instance['title'] = strip_tags( $new_instance['title'] );
-			foreach ( $ngfb->cf['lib']['website'] as $id => $name )
+			foreach ( $this->p->cf['lib']['website'] as $id => $name )
 				$instance[$id] = empty( $new_instance[$id] ) ? 0 : 1;
 			unset( $name, $id );
 			return $instance;
 		}
 	
 		public function form( $instance ) {
-			global $ngfb;
 			$title = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : 'Share It';
 			echo "\n", '<p><label for="', $this->get_field_id( 'title' ), '">Title (Leave Blank for No Title):</label>',
 				'<input class="widefat" id="', $this->get_field_id( 'title' ), 
 					'" name="', $this->get_field_name( 'title' ), 
 					'" type="text" value="', $title, '" /></p>', "\n";
 	
-			foreach ( $ngfb->cf['lib']['website'] as $id => $name ) {
-				$classname = $ngfb->cf['cca'].'Social'.ucfirst( $id );
+			foreach ( $this->p->cf['lib']['website'] as $id => $name ) {
+				$classname = $this->p->cf['cca'].'Social'.ucfirst( $id );
 				if ( class_exists( $classname ) ) {
 					$name = $name == 'GooglePlus' ? 'Google+' : $name;
 					echo '<p><label for="', $this->get_field_id( $id ), '">', 
