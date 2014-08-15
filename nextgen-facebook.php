08b3ae065e9e067ff5cbc951c99b437951fedf34
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-15 22:06:53 +0000

    v7.6.3.1
    git-svn-id: https://plugins.svn.wordpress.org/nextgen-facebook/trunk@966617 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- nextgen-facebook.php
+++ nextgen-facebook.php
@@ -246,7 +246,7 @@ if ( ! class_exists( 'Ngfb' ) ) {
 					if ( $this->is_avail['aop'] === false )
 						$this->notice->inf( 'An Authentication ID was entered for '.$this->short.', 
 						but the Pro version is not installed yet &ndash; 
-						don\'t forget to update '.$this->short.' to install the Pro version.', true );
+						don\'t forget to update the '.$this->short.' plugin to install the Pro version.', true );
 					foreach ( $this->cf['plugin'] as $lca => $info ) {
 						$last_update = get_option( $lca.'_utime' );
 						if ( empty( $last_update ) || 
