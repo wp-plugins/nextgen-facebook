ab11cebf9a4309e125ead05bdd0fd43062ba5f34
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-18 20:29:54 +0000

    v7.6.3.2
    git-svn-id: https://plugins.svn.wordpress.org/nextgen-facebook/tags/7.6.3.2@967860 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/setting/contact.php
+++ lib/setting/contact.php
@@ -21,7 +21,7 @@ if ( ! class_exists( 'NgfbSettingContact' ) && class_exists( 'NgfbSubmenuAdvance
 
 		protected function add_meta_boxes() {
 			// add_meta_box( $id, $title, $callback, $post_type, $context, $priority, $callback_args );
-			add_meta_box( $this->pagehook.'_contact', 'Profile Contact Methods', array( &$this, 'show_metabox_contact' ), $this->pagehook, 'normal' );
+			add_meta_box( $this->pagehook.'_contact', 'Profile Contact Fields', array( &$this, 'show_metabox_contact' ), $this->pagehook, 'normal' );
 		}
 
 	}
