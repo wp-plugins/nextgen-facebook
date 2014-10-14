d5f72996ddb906389b7572fbc4c5cc27dfc74efd
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-14 12:53:23 +0000

    v7.6.12
    git-svn-id: https://plugins.svn.wordpress.org/nextgen-facebook/tags/7.6.12@1007250 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/admin.php
+++ lib/admin.php
@@ -200,7 +200,7 @@ if ( ! class_exists( 'NgfbAdmin' ) ) {
 			$def_opts = $this->p->opt->get_defaults();
 			$opts = SucomUtil::restore_checkboxes( $opts );
 			$opts = array_merge( $this->p->options, $opts );
-			$opts = $this->p->opt->sanitize( $opts, $def_opts );	// cleanup excess options and sanitize
+			$opts = $this->p->opt->sanitize( $opts, $def_opts );
 			$opts = apply_filters( $this->p->cf['lca'].'_save_options', $opts, NGFB_OPTIONS_NAME );
 			$this->p->notice->inf( __( 'Plugin settings have been updated.', NGFB_TEXTDOM ).' '.
 				sprintf( __( 'Wait %d seconds for cache objects to expire (default) or use the \'Clear All Cache\' button.', NGFB_TEXTDOM ), 
