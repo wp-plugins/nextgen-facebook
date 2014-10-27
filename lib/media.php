6f001452684e373ccc90b48688106f0a0a5d3363
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-27 23:20:25 +0000

    v7.6.13.2
    git-svn-id: https://plugins.svn.wordpress.org/nextgen-facebook/trunk@1014947 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/media.php
+++ lib/media.php
@@ -701,8 +701,9 @@ if ( ! class_exists( 'NgfbMedia' ) ) {
 				);
 			}
 
-			if ( empty( $og_video['og:video'] ) && empty( $og_video['og:image'] ) ) 
-				return array();
+			if ( empty( $og_video['og:video'] ) && 
+				empty( $og_video['og:image'] ) ) 
+					return array();
 			else return $og_video;
 		}
 	}
