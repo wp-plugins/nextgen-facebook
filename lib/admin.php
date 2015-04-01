92a86fbf6a277e7ae2c72a8fa8904b76eaa5e597
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-01 20:53:04 +0000

    v7.9 remove some commented code
    git-svn-id: https://plugins.svn.wordpress.org/nextgen-facebook/trunk@1125808 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/admin.php
+++ lib/admin.php
@@ -501,9 +501,6 @@ if ( ! class_exists( 'NgfbAdmin' ) ) {
 						'style="background-color:#0f0;"';
 				}
 
-				//$update_info = class_exists( 'SucomUpdate' ) ?
-				//	SucomUpdate::get_option( $lca ) : false;
-	
 				echo '<tr><td colspan="2"><h4>'.$info['short'].
 					( $this->p->check->aop( $lca ) ? ' Pro' : ' Free' ).'</h4></td></tr>';
 				echo '<tr><th class="side">'.__( 'Installed', NGFB_TEXTDOM ).':</th>
