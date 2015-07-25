a14fd3dc8f2e8a51c13e132f5040124984cf19b3
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-25 23:32:55 +0000

    v8.6.2.1
    git-svn-id: https://plugins.svn.wordpress.org/nextgen-facebook/trunk@1206550 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/config.php
+++ lib/config.php
@@ -20,7 +20,7 @@ if ( ! class_exists( 'NgfbConfig' ) ) {
 			'feed_cache_exp' => 86400,	// 24 hours
 			'plugin' => array(
 				'ngfb' => array(
-					'version' => '8.6.2',		// plugin version
+					'version' => '8.6.2.1',		// plugin version
 					'short' => 'NGFB',		// short plugin name
 					'name' => 'NextGEN Facebook (NGFB)',
 					'desc' => 'Display your content in the best possible way on Facebook, Google+, Twitter, Pinterest, etc. - no matter how your webpage is shared!',
@@ -498,7 +498,7 @@ if ( ! class_exists( 'NgfbConfig' ) ) {
 				),
 			),
 			'php' => array(				// php
-				'min_version' => '4.0.6',	// minimum php version
+				'min_version' => '4.1.0',	// minimum php version
 			),
 			'follow' => array(
 				'size' => 32,
